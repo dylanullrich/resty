94a2f190c7f0216eece220a41553e747c485584a
DYLAN ULLRICH <DYLAN@GETULLRICHORDIETRYING.COM>
2022-07-19 15:38:53 -0700

    prepped for lab

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -4,7 +4,11 @@ class Results extends React.Component {
   render() {
     return (
       <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+        <pre>
+          {this.props.data
+            ? JSON.stringify(this.props.data, undefined, 2)
+            : null}
+        </pre>
       </section>
     );
   }
